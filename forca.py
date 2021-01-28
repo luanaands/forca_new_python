fe9b10aa77d930ecc14af6c0d5d31d55d1099f3d
luanaands <luanaands@gmail.com>
2021-01-27 21:20:21 -0300

    inicio

---
+++ forca.py
@@ -0,0 +1,28 @@
+# fixo 
+# chances = 5
+# chute  
+cont = 0
+palavraFixa = input()
+tamanho = len(palavraFixa)
+palavraAuxiliar = '-' * tamanho
+print("Descubra a palavra {}".format(palavraAuxiliar))
+letra = input()
+index = palavraFixa.find(letra)
+
+
+"""
+QuatidadeDeChances = 5
+while True:
+   
+    print('-' * tamanho)
+
+
+    for i in range(QuatidadeDeChances):
+        print('Sua chance atual é {}'.format(QuatidadeDeChances-i))
+        letra = input()
+        
+   """
+
+
+  
+
